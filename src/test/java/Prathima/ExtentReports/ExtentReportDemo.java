268b1551d7538133ad11b9d2c8d649d774ea0e6c
prathimasuvarna11 <prathimasuvarna2@gmail.com>
2020-07-26 12:13:36 +0530

    Extent Report

---
+++ src/test/java/Prathima/ExtentReports/ExtentReportDemo.java
@@ -0,0 +1,41 @@
+package Prathima.ExtentReports;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportDemo {
+	ExtentReports extent;
+	@BeforeTest
+	public void config()
+	{
+		String path=System.getProperty("user.dir")+"\\reports\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Prathima");
+	}
+	
+	
+	@Test
+	public void initialDemo()
+	{
+		ExtentTest test= extent.createTest("initialDemo");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\NI7587\\Desktop\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+		test.fail("Result do not match");
+		
+		extent.flush();
+	}
+
+}
